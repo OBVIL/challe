632e3fd3f186a4acba5862bc413cb8adebca3e77
glorieux-f <frederic.glorieux@fictif.org>
2017-09-11 15:13:47 +0200

    install testée

--- _conf.php
+++ _conf.php
@@ -1,12 +1,10 @@
 <?php
 return array(
-  "title" => 'Góngora', // Nom du corpus
+  "title" => 'Robert Challe', // Nom du corpus
   "srcdir" => dirname( __FILE__ ), // dossier source depuis lequel exécuter la commande de mise à jour
-  "cmdup" => "git submodule foreach git pull origin gh-pages 2>&1", // entrepôt avec modules
-  "pass" => null, // Mot de passe à renseigner à l’installation
-  "srcglob" => array( "polemos/*.xml", "gongoraobra/gongora_obra-poetica.xml" ), // pour mise à jour de la polémique
-  "sqlite" => "polemos.sqlite", // nom de la base avec les métadonnées
-  "destdir" => ".", // dossier de base où écrire les fichiers produits
-  "formats" => "article, toc, epub, kindle", // formats à générer
+  "cmdup" => "git pull 2>&1", // commande de mise à jour
+  "pass" => null, // Mot de passe à renseigner obligatoirement à l’installation
+  "srcglob" => array( "xml/challe_illustres-francaises.xml" ), // sources XML à publier
+  "sqlite" => "challe.sqlite", // nom de la base avec les métadonnées
 );
 ?>
